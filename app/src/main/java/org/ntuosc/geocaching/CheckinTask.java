f0421fc49b31224ade7c77ad139e7d33a3c5bdd5
Poren Chiang <ren.chiang@gmail.com>
2014-08-23 07:39:28 +0800

    Refactor checkin code, eliminating for loop

--- app/src/main/java/org/ntuosc/geocaching/CheckinTask.java
+++ app/src/main/java/org/ntuosc/geocaching/CheckinTask.java
@@ -46,105 +46,101 @@ public class CheckinTask extends AsyncTask<Tag, Integer, CheckinTask.Result> {
             return new Result(CODE_ENDPOINT_INCORRECT);
         }
 
-        // Iterate through available tags. Presumably one.
-        for (Tag tag : tags) {
+        // Get available tag. We assume there is only one per task.
+        Tag tag = tags[0];
 
-            // Build up arguments
-            HashMap<String, String> params = new HashMap<String, String>();
-            String uid = Util.toHexString(tag.getId());
+        // Build up arguments
+        HashMap<String, String> params = new HashMap<String, String>();
+        String uid = Util.toHexString(tag.getId());
 
-            params.put("auth", mEndpointKey);
-            params.put("cuid", uid);
+        params.put("auth", mEndpointKey);
+        params.put("cuid", uid);
 
-            // Build up URL
-            URL url;
+        // Build up URL
+        URL url;
 
-            try {
-                url = new URL(String.format(Locale.getDefault(), URL_ENDPOINT, mEndpointName));
-            }
-            catch (MalformedURLException ex) {
-                Log.e(PACKAGE_NAME, "URL formatting failed", ex);
-                return new Result(CODE_ENDPOINT_INCORRECT);
-            }
-
-            // Send request
-            HttpsURLConnection request = null;
-            String response = null;
-
-            try {
-                request = (HttpsURLConnection) url.openConnection();
-
-                request.setUseCaches(false);
-                request.setDoInput(true);
-                request.setDoOutput(true);
-                request.setRequestMethod("POST");
-                request.setRequestProperty("X-Geocaching-Client", "NTUOSC");
-                request.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-                OutputStream output = request.getOutputStream();
-                Writer writer = new BufferedWriter(new OutputStreamWriter(output, DEFAULT_ENCODING));
-                writer.write(Util.toQueryString(params));
-                writer.flush();
-                writer.close();
-                output.close();
-
-                response = Util.readToEnd(request.getInputStream());
-            }
-            catch (IOException ex) {
-                Log.e(PACKAGE_NAME, "Error occurred while building up connection", ex);
-                ex.printStackTrace();
-
-                if (request != null) {
-                    try {
-                        // Read HTTP Error Code
-                        Log.v(PACKAGE_NAME, String.format(Locale.getDefault(),
-                                "HTTP %d: %s",
-                                request.getResponseCode(), request.getResponseMessage()));
-
-                        // Try parse and read server response
-                        response = Util.readToEnd(request.getErrorStream());
-                        JSONObject entity = new JSONObject(response);
-
-                        Log.v(PACKAGE_NAME, String.format(Locale.getDefault(),
-                                "Error message: %s", entity.getString("message")));
-
-                        // Return appropriate result code
-                        if (request.getResponseCode() == HTTP_BAD_REQUEST)
-                            return new Result(CODE_ENDPOINT_INCORRECT);
-                        else if (request.getResponseCode() == HTTP_INTERNAL_ERROR)
-                            return new Result(CODE_GENERIC_ERROR);
-                    }
-                    catch (Exception inner) {
-                        Log.e(PACKAGE_NAME, "Failed to parse error response", inner);
-                        inner.printStackTrace();
-                    }
-                }
-
-                // Huston, we've got a problem
-                return new Result(CODE_NETWORK_ERROR);
-            }
-
-            // Parse JSON and read data
-            try {
-                JSONObject entity = new JSONObject(response);
-
-                Result result = new Result(CODE_SUCCESS);
-                result.checkin_count = entity.getInt("count");
-                result.registered = (entity.getInt("is_registered") > 0);
-                result.redeemable = (entity.getInt("can_redeem") > 0);
-
-                Log.v(PACKAGE_NAME, "Checkin success!");
-                return result;
-            }
-            catch (JSONException ex) {
-                Log.e(PACKAGE_NAME, "Failed to parse response", ex);
-                ex.printStackTrace();
-
-                return new Result(CODE_GENERIC_ERROR);
-            }
+        try {
+            url = new URL(String.format(Locale.getDefault(), URL_ENDPOINT, mEndpointName));
+        }
+        catch (MalformedURLException ex) {
+            Log.e(PACKAGE_NAME, "URL formatting failed", ex);
+            return new Result(CODE_ENDPOINT_INCORRECT);
         }
 
-        return new Result(CODE_SUCCESS);
+        // Send request
+        HttpsURLConnection request = null;
+        String response = null;
+
+        try {
+            request = (HttpsURLConnection) url.openConnection();
+
+            request.setUseCaches(false);
+            request.setDoInput(true);
+            request.setDoOutput(true);
+            request.setRequestMethod("POST");
+            request.setRequestProperty("X-Geocaching-Client", "NTUOSC");
+            request.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            OutputStream output = request.getOutputStream();
+            Writer writer = new BufferedWriter(new OutputStreamWriter(output, DEFAULT_ENCODING));
+            writer.write(Util.toQueryString(params));
+            writer.flush();
+            writer.close();
+            output.close();
+
+            response = Util.readToEnd(request.getInputStream());
+        }
+        catch (IOException ex) {
+            Log.e(PACKAGE_NAME, "Error occurred while building up connection", ex);
+            ex.printStackTrace();
+
+            if (request != null) {
+                try {
+                    // Read HTTP Error Code
+                    Log.v(PACKAGE_NAME, String.format(Locale.getDefault(),
+                            "HTTP %d: %s",
+                            request.getResponseCode(), request.getResponseMessage()));
+
+                    // Try parse and read server response
+                    response = Util.readToEnd(request.getErrorStream());
+                    JSONObject entity = new JSONObject(response);
+
+                    Log.v(PACKAGE_NAME, String.format(Locale.getDefault(),
+                            "Error message: %s", entity.getString("message")));
+
+                    // Return appropriate result code
+                    if (request.getResponseCode() == HTTP_BAD_REQUEST)
+                        return new Result(CODE_ENDPOINT_INCORRECT);
+                    else if (request.getResponseCode() == HTTP_INTERNAL_ERROR)
+                        return new Result(CODE_GENERIC_ERROR);
+                }
+                catch (Exception inner) {
+                    Log.e(PACKAGE_NAME, "Failed to parse error response", inner);
+                    inner.printStackTrace();
+                }
+            }
+
+            // Huston, we've got a problem
+            return new Result(CODE_NETWORK_ERROR);
+        }
+
+        // Parse JSON and read data
+        try {
+            JSONObject entity = new JSONObject(response);
+
+            Result result = new Result(CODE_SUCCESS);
+            result.checkin_count = entity.getInt("count");
+            result.registered = (entity.getInt("is_registered") > 0);
+            result.redeemable = (entity.getInt("can_redeem") > 0);
+
+            Log.v(PACKAGE_NAME, "Checkin success!");
+            return result;
+        }
+        catch (JSONException ex) {
+            Log.e(PACKAGE_NAME, "Failed to parse response", ex);
+            ex.printStackTrace();
+            return new Result(CODE_GENERIC_ERROR);
+        }
     }
 
     @Override
